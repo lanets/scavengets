083aabca9ee53e62ae978b51b6e53d747fc51b50
Bach Nguyen-Ngoc <bnguyen@lanets.ca>
2017-05-27 20:30:25 -0400

    added error module to authentication service

--- angular-client/src/app/auth/auth.service.ts
+++ angular-client/src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import {Injectable} from "@angular/core";
 import {Http, Headers, Response} from "@angular/http";
 import 'rxjs/Rx';
 import {Observable} from "rxjs/Observable";
+import {ErrorService} from "../errors/error.service";
 
 @Injectable()
 
@@ -10,14 +11,17 @@ export class AuthService {
   // Link to our api, pointing to localhost
   API = 'http://localhost:3000';
 
-  constructor(private http: Http){}
+  constructor(private http: Http, private errorService: ErrorService){}
 
   signup(user: User){
     const body = JSON.stringify(user);
     const headers = new Headers({'Content-Type':'application/json'});
     return this.http.post(`${this.API}/user`, body, {headers: headers})
       .map((response: Response) => response.json())
-      .catch((error: Response) => Observable.throw(error.json()));
+      .catch((error: Response) => {
+        this.errorService.handleError(error.json());
+        return Observable.throw(error.json());
+      });
   }
 
   signin(user: User){
@@ -25,7 +29,10 @@ export class AuthService {
     const headers = new Headers({'Content-Type': 'application/json'});
     return this.http.post(`${this.API}/user/signin`, body, {headers: headers})
       .map((response: Response) => response.json())
-      .catch((error: Response) => Observable.throw(error.json()));
+      .catch((error: Response) => {
+        this.errorService.handleError(error.json());
+        return Observable.throw(error.json());
+      });
   }
 
   logout(){
