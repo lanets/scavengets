aa0c0c9feb46e3d39be9645899d8934be42a088e
Shuhala <sophiebmercier@hotmail.com>
2017-05-30 23:10:11 -0400

    Cleaned app routing imports

--- angular-client/src/app/app.routing.ts
+++ angular-client/src/app/app.routing.ts
@@ -1,22 +1,24 @@
-import {RouterModule, Routes, CanActivate} from "@angular/router";
+import { RouterModule, Routes } from '@angular/router';
 
-import {AuthenticationComponent} from "./modules/authentication/authentication.component";
-import {TeamsComponent} from "./modules/teams/teams.component";
-import {ChallengesComponent} from "./modules/challenges/challenges.component";
-import {HomeComponent} from "./modules/home/home.component";
-import {JudgeComponent} from "./modules/judge/judge.component";
-import {AUTH_ROUTES} from "./modules/authentication/auth.routes";
-import {CanActivateAuthentificationGuard} from "./shared/services/route-guards/authentication-guard";
-import {AccountComponent} from "./modules/authentication/account/account.component"
+import { AuthenticationComponent, AccountComponent } from '@modules/authentication';
+import { AUTH_ROUTES } from '@modules/authentication/auth.routes';
+
+import { ChallengesComponent } from '@modules/challenges';
+import { HomeComponent } from '@modules/home';
+import { JudgeComponent } from '@modules/judge';
+import { TeamsComponent } from '@modules/teams';
+
+import { CanActivateAuthentificationGuard } from '@route-guards';
+
 
 const APP_ROUTES: Routes = [
-  { path: '',redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent},
-  { path: 'teams', component: TeamsComponent},
-  { path: 'challenges', component: ChallengesComponent},
-  { path: 'judge', component: JudgeComponent},
-  { path: 'auth', component: AuthenticationComponent, children: AUTH_ROUTES},
-  { path: 'account', component: AccountComponent, canActivate:[CanActivateAuthentificationGuard] }
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'teams', component: TeamsComponent },
+  { path: 'challenges', component: ChallengesComponent },
+  { path: 'judge', component: JudgeComponent },
+  { path: 'auth', component: AuthenticationComponent, children: AUTH_ROUTES },
+  { path: 'account', component: AccountComponent, canActivate: [CanActivateAuthentificationGuard] }
 ];
 
 export const Routing = RouterModule.forRoot(APP_ROUTES);
