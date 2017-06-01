95efa0136a3335fa4ecd30c7f0bcff1d957a9cc6
Shuhala <sophiebmercier@hotmail.com>
2017-05-31 21:04:33 -0400

    Added documentation for /user endpoint

--- express-server/routes/users.js
+++ express-server/routes/users.js
@@ -13,59 +13,35 @@ router.get('/', getUsersList);
 // router.put('/:_id', update);
 // router.delete('/:_id', _delete);
 
-function register (req, res, next) {
-  var user = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    userName: req.body.userName,
-    password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-  });
-
-  user.save(function (err, result) {
-    if (err) {
-      return res.status(500).json({
-        title: 'An error occured',
-        error: err
-      })
-    }
-    res.status(201).json({
-      message: 'User created',
-      obj: result
-    })
-  })
-}
-
-function authenticate (req, res, next) {
-  User.findOne({userName: req.body.userName}, function (err, user) {
-    if (err) {
-      return res.status(500).json({
-        title: 'An error occurred',
-        error: err
-      })
-    }
-    if (!user) {
-      return res.status(401).json({
-        title: 'no user found', // temporary for debugging
-        error: {message: 'user could not be found'}
-      })
-    }
-    if (!bcrypt.compareSync(req.body.password, user.password)) {
-      return res.status(401).json({
-        title: 'Login failed',
-        error: {message: 'Invalid login credentials'}
-      })
-    }
-    // expiresIn in seconds, so currently set for 2 hours
-    var token = jwt.sign({user: user}, 'jwtSecret', {expiresIn: 7200});
-    res.status(200).json({
-      message: 'Successfully logged in',
-      token: token,
-      userId: user._id
-    })
-  })
-}
-
-function getUsersList(req, res, next) {
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *     description: Returns users list
+ *     produces:
+ *      - application/json
+ *     responses:
+ *       200:
+ *         description: users
+ *         schema:
+ *           type: array
+ *         examples:
+ *           application/json: {
+ *             "2412412": [
+ *               "username": "caflamand",
+ *               "firstname": "Rose",
+ *               "lastname": "Lamant"
+ *             ],
+ *             "89732523": [
+ *               "username": "bacon",
+ *               "firstname": "Laurent",
+ *               "lastname": "Tremblay"
+ *             ]
+ *           }
+ *       409:
+ *         description: Access forbidden
+ */
+function getUsersList(res, next) {
     User.find({}, function(err, users) {
         var usersList = {};
 
@@ -81,50 +57,103 @@ function getUsersList(req, res, next) {
     });
 }
 
+function register (req, res, next) {
+    var user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        userName: req.body.userName,
+        password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+    });
+
+    user.save(function (err, result) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occured',
+                error: err
+            })
+        }
+        res.status(201).json({
+            message: 'User created',
+            obj: result
+        })
+    })
+}
+
+function authenticate (req, res, next) {
+    User.findOne({userName: req.body.userName}, function (err, user) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            })
+        }
+        if (!user) {
+            return res.status(401).json({
+                title: 'no user found', // temporary for debugging
+                error: {message: 'user could not be found'}
+            })
+        }
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
+            return res.status(401).json({
+                title: 'Login failed',
+                error: {message: 'Invalid login credentials'}
+            })
+        }
+        // expiresIn in seconds, so currently set for 2 hours
+        var token = jwt.sign({user: user}, 'jwtSecret', {expiresIn: 7200});
+        res.status(200).json({
+            message: 'Successfully logged in',
+            token: token,
+            userId: user._id
+        })
+    })
+}
+
+
 module.exports = router;
 
 /*
-function getAll(req, res) {
-    userService.getAll()
-        .then(function (users) {
-            res.send(users);
-        })
-        .catch(function (err) {
-            res.status(400).send(err);
-        });
-}
+ function getAll(req, res) {
+ userService.getAll()
+ .then(function (users) {
+ res.send(users);
+ })
+ .catch(function (err) {
+ res.status(400).send(err);
+ });
+ }
 
-function getCurrent(req, res) {
-    userService.getById(req.user.sub)
-        .then(function (user) {
-            if (user) {
-                res.send(user);
-            } else {
-                res.sendStatus(404);
-            }
-        })
-        .catch(function (err) {
-            res.status(400).send(err);
-        });
-}
+ function getCurrent(req, res) {
+ userService.getById(req.user.sub)
+ .then(function (user) {
+ if (user) {
+ res.send(user);
+ } else {
+ res.sendStatus(404);
+ }
+ })
+ .catch(function (err) {
+ res.status(400).send(err);
+ });
+ }
 
-function update(req, res) {
-    userService.update(req.params._id, req.body)
-        .then(function () {
-            res.sendStatus(200);
-        })
-        .catch(function (err) {
-            res.status(400).send(err);
-        });
-}
+ function update(req, res) {
+ userService.update(req.params._id, req.body)
+ .then(function () {
+ res.sendStatus(200);
+ })
+ .catch(function (err) {
+ res.status(400).send(err);
+ });
+ }
 
-function _delete(req, res) {
-    userService.delete(req.params._id)
-        .then(function () {
-            res.sendStatus(200);
-        })
-        .catch(function (err) {
-            res.status(400).send(err);
-        });
-}
-*/
+ function _delete(req, res) {
+ userService.delete(req.params._id)
+ .then(function () {
+ res.sendStatus(200);
+ })
+ .catch(function (err) {
+ res.status(400).send(err);
+ });
+ }
+ */
